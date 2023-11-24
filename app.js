f8abeb883f882e6ee593c0fb290d1668aec70748
shnlmn <magnetscoil@gmail.com>
2023-11-23 20:38:49 -0800

    Added express framework to manage routing and serial input

--- app.js
+++ app.js
@@ -1,32 +1,117 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const server  = require('node:http').createServer(app);
-const io = require('socket.io')(server);
+const server = require("node:http").createServer(app);
+const io = require("socket.io")(server);
 // const path = require('path');
-const { SerialPort } = require('serialport');
-const { ReadlineParser } = require('@serialport/parser-readline');
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-        console.log('user disconnected')
-        })
-    });
+// 0000000000000000000000000000000000000000000000
+//                     Serial Port
+const { SerialPort } = require("serialport");
+const { ReadlineParser } = require("@serialport/parser-readline");
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
 
 const port = new SerialPort({
-    path: '/dev/ttyUSB0',
-    baudRate: 115200,
-    })
+  path: "/dev/ttyUSB0",
+  baudRate: 115200,
+});
 
-const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
+const parser = port.pipe(new ReadlineParser({ delimiter: "\r\n" }));
 
-parser.on('data', function (data) {
-    // console.log(data)
-    io.emit('serialData', data)
-    })
+parser.on("data", function (data) {
+  // console.log(data)
+  io.emit("serialData", data);
+});
+// 00000000000000000000000000000000000000000000000000
 
-app.use(express.static('./public'));
+// 111111111111111111111111111111111111111111111111111
+//                     Express
 
+// Express setup
+app.use(express.static("./public"));
+// app.engine('html', require('ejs').renderFile);
+// app.set('view engine', 'ejs');
+
+const base_scripts = [
+  "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js",
+  "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js",
+  "https://cdn.socket.io/socket.io-3.0.5.min.js",
+  "/lib/p5.js",
+  "/lib/p5play.min.js",
+  "/lib/planck.min.js",
+  "/src/sketch.js",
+  "/src/pegboard.js",
+];
+
+const games = [
+  { name: "Batter Up!", script: "ball_bats.js" },
+  { name: "Blasters!", script: "ball_cannon.js" },
+  { name: "Pingball", script: "ball.js" },
+  { name: "Trampoline!", script: "bubble_bounce.js" },
+  { name: "Word Jumble", script: "spelling_braille.js" },
+  { name: "Ambient Background", script: "ambient.js" },
+  { name: "Peg Mapping", script: "peg_map.js" },
+];
+
+// Render the landing page //
+app.get("/", (req, res) => {
+  res.send(renderLandingPage());
+});
+
+function renderLandingPage() {
+  const gameLinks = games.map(
+    (game) => `<li><a href="/run-script/${game.script}">${game.name}</a></la>`
+  );
+  return `
+        <html>
+            <head>
+                <title>Pegboard Games</title>
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+    /> <link rel="stylesheet" type="text/css" href="../style.css" />
+            </head>
+            <body>
+                <h1>Pegboard Games</h1>
+                <ul>
+                    ${gameLinks.join("")}
+                </ul>
+            </body>
+        </html>
+    `;
+}
+
+// Route handler for each game //
+app.get("/run-script/:script", (req, res) => {
+  const selectedScript = req.params.script;
+  const scriptsToInclue = [...base_scripts, `/games/${selectedScript}`];
+  res.send(renderHtmlTemplate(selectedScript, scriptsToInclue));
+});
+
+function renderHtmlTemplate(selectedScript,scriptsToInclude) {
+  const scriptTags = scriptsToInclude.map(
+    (script) => `<script src="${script}" type="text/javascript"></script>`
+  );
+  return `
+        <html>
+            <head>
+                <title>Pegboard Games</title>
+                ${scriptTags.join("")}
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" /> 
+                <link rel="stylesheet" type="text/css" href="../style.css" />
+            </head>
+            <body>
+                <h1>${selectedScript}</h1>
+                <div id="sketch-holder"></div>
+            </body>
+        </html>
+    `;
+}
 server.listen(5000, () => {
-    console.log('Server is running on port 5000');
+  console.log("Server is running on port 5000");
 });
---
+++ public/games/ball_bats.js
@@ -0,0 +1,64 @@
+class pbAnimation extends Pegboard {
+  constructor() {
+    super();
+    this.speed = 0.3 * this.scale;
+    this.speed_floor = 20 * this.scale;
+    this.circle_distance = 4 * this.scale;
+    this.ball_diameter = 4 * this.board_peg_size;
+    this.location = createVector(0, 0);
+    this.travel_vector = createVector(0, 0);
+    this.to_node = 0;
+    this.start_point = 0;
+    this.active_count = 0;
+    this.peg_coords = this.get_peg_coords();
+    this.paddles = new Group();
+    this.balls = new Group();
+    world.gravity.y = 1;
+    for (let i = 0; i < 100; i++) {
+      let x = ~~(Math.random() * this.display_w);
+      let y = ~~(Math.random() * this.display_h);
+      let ball = new this.balls.Sprite(x, y, this.board_peg_size * 4);
+      ball.strokeWeight = 0;
+      ball.friction = 0.01;
+    }
+  }
+
+  display() {
+    background(100, 100);
+    this.paddles.draw();
+    this.balls.draw();
+    if (frameCount % 5 == 0) {
+      new this.balls.Sprite(~~(Math.random() * this.display_w), -150).d =
+        this.board_peg_size * 4;
+      this.balls.strokeWeight = 0;
+      this.balls.friction = 0.01;
+    }
+  }
+  preload(){
+
+  }
+  ///// detect mouse click, find closest peg, and add a sprite
+  mouseClicked(e) {
+    let peg_obj = this.toggle_peg();
+    if (peg_obj) {
+      const coords = peg_D_obj['coord']
+      let new_peg = true;
+      for (let index = 0; index < this.paddles.length; index++) {
+        const paddle = this.paddles[index];
+        if (paddle.peg_number == peg_obj["number"]) {
+          paddle.remove();
+          new_peg = false;
+        }
+      }
+      if (new_peg) {
+        let paddle = new this.paddles.Sprite(coords.x, coords.y, 80, 10, "k");
+        paddle.color = "red";
+        paddle.strokeWeight = 0;
+        paddle.offset.x = 30;
+        const direction = Math.random()<0.5 ? 1 : -1;
+        print(direction)
+        paddle.rotationSpeed = direction * 10;
+      }
+    }
+  }
+}
